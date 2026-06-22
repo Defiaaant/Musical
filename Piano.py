d3d5e7c6e0a02c5bc48893efc10492b77ebd3ea7
tiagoschalegre-oss <tiagoschalegre@gmail.com>
2026-06-22 16:36:45 -0300

    Update Piano.py

--- Piano.py
+++ Piano.py
@@ -1,102 +1,127 @@
 from tkinter import*
+from tkinter import ttk
+import os
+
 import pygame
 #----------------------------------------------------------------------
-def Do():
-  nota = pygame.mixer.Sound("Do.wav")
-  nota.play()
-#----------------------------------------------------------------------
-def Re():
-  nota = pygame.mixer.Sound("Re.wav")
-  nota.play()
-#----------------------------------------------------------------------
-def Mi():
-  nota = pygame.mixer.Sound("Mi.wav")
-  nota.play()
-#----------------------------------------------------------------------
-def Fa():
-  nota = pygame.mixer.Sound("Fa.wav")
-  nota.play()
-#----------------------------------------------------------------------
-def Sol():
-  nota = pygame.mixer.Sound("Sol.wav")
-  nota.play()
-#----------------------------------------------------------------------
-def La():
-  nota = pygame.mixer.Sound("La.wav")
-  nota.play()
-#----------------------------------------------------------------------
-def Si():
-  nota = pygame.mixer.Sound("Si.mp3")
-  nota.play()
-#----------------------------------------------------------------------
-def Do_():
-  nota = pygame.mixer.Sound("Do (octave).wav")
+def tocar_som(entrada):
+  nota = pygame.mixer.Sound(entrada)
   nota.play()
 
+def seletor(event):
+  janela = Toplevel()
+  janela.config(background='#484886')
+  janela.geometry('700x500')
+
+  retorno=Button(janela, image=voltar, width=30, height=30,
+                 bg='#484886',
+                 activebackground="#484886", activeforeground="#484886",
+                 relief='flat',
+                 command=lambda: janela.destroy())
+  retorno.pack(anchor='nw')
+
+  Label(janela, text="Aperte no botão da música que quer escutar", image=img, compound='top',
+        font=("Arial",12, 'bold'),
+        fg="#ffffff", bg='#484886').pack(anchor='center')
+
+  frame2=Frame(janela, relief=FLAT)
+
+  b1=Button(frame2, text= 'musga q tiago vai por (1)', command=NONE).pack(side='bottom')
+  b2=Button(frame2, text= 'musga q tiago vai por (2)', command=NONE).pack(side='bottom')
+  b3=Button(frame2, text= 'musga q tiago vai por (3)', command=NONE).pack(side='bottom')
+  b4=Button(frame2, text= 'musga q tiago vai por (4)', command=NONE).pack(side='bottom')
+
+  frame2.pack(padx=100, pady=100)
+
 pygame.mixer.init()
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+notas = os.path.join(base_dir, "Notas")
 
 #§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§
 
 root = Tk()
-root.geometry('600x400')
+root.geometry('700x500')
 
-img = PhotoImage(file='piano.png')
+img = PhotoImage(file='pyano.png')
+voltar = PhotoImage(file='voltar.png')
+lista=PhotoImage(file='lista.png')
+
+salvar=Label(root, image=lista, width=50, height=50, bg='#484886')
+salvar.pack(anchor='nw')
+salvar.bind("<Button-1>", seletor)
+
+tecla1=PhotoImage(file='tecla2.png')
+tecla2=PhotoImage(file='tecla3.png')
+tecla3=PhotoImage(file='tecla1.png')
 preta = PhotoImage(file='t_preta.png')
+
 root.iconphoto(True, img)
 
 root.config(background="#484886")
 
-Do = Button(root, image=preta,
-             bg='#000000',
-             width=30, height=130,
-             activebackground='#000000', activeforeground='#000000',
-             command=Do).pack(side=LEFT)
+frame=Frame(root, relief=FLAT)
+
+Pyano=Label(root, image=img,
+            text ='PYANO',
+            font=('Lato', 50),
+            background="#484886",
+            compound='right',
+            border=4, padx= 4, pady= 4,
+            relief='flat')
+Pyano.pack(anchor='center')
+#Pyano.bind("<Button-1>", seletor)
+
 #------------------------------------------
-Re = Button(root, image=preta,
-             bg='#000000',
+Do = Button(frame, image=tecla1,
              width=30, height=130,
-             activebackground='#000000', activeforeground='#000000',
-             command=Re).pack(side=LEFT)
+             bg='#484886' ,activebackground="#484886", activeforeground="#484886",
+             relief='flat',
+             command=lambda :tocar_som(os.path.join(notas, "Do.wav"))).pack(side=LEFT)
 #------------------------------------------
-Mi = Button(root, image=preta,
-             bg='#000000',
+Re = Button(frame, image=tecla2,
              width=30, height=130,
-             activebackground='#000000', activeforeground='#000000',
-             command=Mi).pack(side=LEFT)
+             bg='#484886' ,activebackground="#484886", activeforeground="#484886",
+             relief='flat',
+             command=lambda :tocar_som(os.path.join(notas, "Re.wav"))).pack(side=LEFT)
 #------------------------------------------
-Fa = Button(root, image=preta,
-             bg='#000000',
+Mi = Button(frame, image=tecla3,
              width=30, height=130,
-             activebackground='#000000', activeforeground='#000000',
-             command=Fa).pack(side=LEFT)
+             bg='#484886' ,activebackground="#484886", activeforeground="#484886",
+             relief='flat',
+             command=lambda :tocar_som(os.path.join(notas, "Mi.wav"))).pack(side=LEFT)
 #------------------------------------------
-Sol = Button(root, image=preta,
-             bg='#000000',
+Fa = Button(frame, image=tecla1,
              width=30, height=130,
-             activebackground='#000000', activeforeground='#000000',
-             command=Sol).pack(side=LEFT)
+             bg='#484886' ,activebackground="#484886", activeforeground="#484886",
+             relief='flat',
+             command=lambda :tocar_som(os.path.join(notas, "Fa.wav"))).pack(side=LEFT)
 #------------------------------------------
-La = Button(root, image=preta,
-             bg='#000000',
+Sol = Button(frame, image=tecla2,
              width=30, height=130,
-             activebackground='#000000', activeforeground='#000000',
-             command=La).pack(side=LEFT)
+             bg='#484886' ,activebackground="#484886", activeforeground="#484886",
+             relief='flat',
+             command=lambda :tocar_som(os.path.join(notas, "Sol.wav"))).pack(side=LEFT)
 #------------------------------------------
-Si = Button(root, image=preta,
-             bg='#000000',
+La = Button(frame, image=tecla3,
              width=30, height=130,
-             activebackground='#000000', activeforeground='#000000',
-             command=Si).pack(side=LEFT)
+             bg='#484886' ,activebackground="#484886", activeforeground="#484886",
+             relief='flat',
+             command=lambda :tocar_som(os.path.join(notas, "La.wav"))).pack(side=LEFT)
 #------------------------------------------
-Do_ = Button(root, image=preta,
-             bg='#000000',
+Si = Button(frame, image=tecla1,
+             width=30, height=130, 
+             bg='#484886' ,activebackground="#484886", activeforeground="#484886",
+             relief='flat',
+             command=lambda :tocar_som(os.path.join(notas, "Si.mp3"))).pack(side=LEFT)
+#------------------------------------------
+Do_ = Button(frame, image=tecla3,
              width=30, height=130,
-             activebackground='#000000', activeforeground='#000000',
-             command=Do_).pack(side='left')
+             bg='#484886' ,activebackground="#484886", activeforeground="#484886",
+             relief='flat',
+             command=lambda :tocar_som(os.path.join(notas, "Do#.wav"))).pack(side=LEFT)
 #------------------------------------------
 
-teclas= Frame(root)
-
-teclas.pack()
+frame.pack(padx=100, pady=100)
 
 root.mainloop()
